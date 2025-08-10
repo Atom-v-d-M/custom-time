e3f59d8059c75d2df7c7dfb3d419325245df7929
Atom-v-d-M <atom.vandermerwe@gmail.com>
2025-08-10 01:19:09 -0700

    Added basic time settings page that lets you select your timer duration

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,48 @@
-import styles from "./page.module.css";
+"use client";
 
-export default function Home() {
+import PrimaryLayout from "@/components/layouts/primaryLayout";
+import styles from "./page.module.scss";
+import { useState } from "react";
+
+export default function TimeSettingsPage() {
+
+  const [timeInputValue, setTimeInputValue] = useState<number | ''>(55)
+  
   return (
-    <div className={styles.page}>
-      asdf
-    </div>
+    <PrimaryLayout>
+      <div className={styles.timeSettingsPage}>
+        <div className={styles.timeSettingsPage__contentContainer}>
+          <h1 className={styles.timeSettingsPage__title}>Timer Duration</h1>
+            <input 
+              type="number" 
+              placeholder="00"
+              value={timeInputValue}
+              min={1}
+              max={180}
+              onChange={(e) => {
+                const value = e.target.value;
+                
+                // Allow empty value
+                if (value === '') {
+                  setTimeInputValue('');
+                  return;
+                }
+                
+                const numberValue = parseFloat(value);
+                
+                // Check if it's a valid number and within range
+                if (!isNaN(numberValue) && numberValue >= 1 && numberValue <= 180) {
+                  setTimeInputValue(numberValue);
+                }
+              }}  
+              className={styles.timeSettingsPage__timerInput}
+            />
+          <button
+            disabled={!timeInputValue}
+            className={styles.timeSettingsPage__startTimerButton}
+          >Start</button>
+        </div>
+      </div>
+    </PrimaryLayout>
   );
 }
